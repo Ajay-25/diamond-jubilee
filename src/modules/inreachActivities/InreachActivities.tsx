fd90c2d1497f5bc9df02d27880e0166552f6efa3
Ajay Kumar <ajay.kumar@sprinklr.com>
2021-09-14 00:35:39 +0530

    Correct link for Inreach Activities

--- src/modules/inreachActivities/InreachActivities.tsx
+++ src/modules/inreachActivities/InreachActivities.tsx
@@ -3,10 +3,8 @@ import React from 'react';
 //components
 import { Video } from '../../components/video';
 
-const URL = 'https://vimeo.com/603676553/590a8ec8d1';
+const URL = 'https://vimeo.com/603662226/1da9e401bf';
 
-const InreachActivities = () => (
-  <Video src={URL} title="Inreach Activities" />
-);
+const InreachActivities = () => <Video src={URL} title="Inreach Activities" />;
 
 export { InreachActivities };
