227ee786fb22d7c68d2dee2b64ff9a5f6dcbbf5b
Ajay Kumar <ajay.kumar@sprinklr.com>
2021-09-13 16:35:51 +0530

    Fixed iframe CSS

--- src/components/video/Video.tsx
+++ src/components/video/Video.tsx
@@ -1,3 +1,4 @@
+//@ts-nocheck
 import React from 'react';
 import Vimeo from '@u-wave/react-vimeo';
 
@@ -10,11 +11,35 @@ const DefaultProps = {
   title: 'Vimeo Video',
 };
 
+const CONTAINER_STYLES = {
+  position: 'relative',
+  height: '100%',
+};
+
+const IFRAME_STYLES = {
+  width: '100%',
+  height: '100vh',
+};
+
 const Video = (props: Props) => {
   const { src, title } = props;
+  const link = src.split('/');
+
+  const head = link[3];
+  const tail = link[4];
 
   return (
-    <Vimeo video={src} id={title} responsive autoplay showByline={false} loop />
+    <div style={CONTAINER_STYLES}>
+      <iframe
+        src={`https://player.vimeo.com/video/${head}?byline=0&amp;speed=0&amp;autoplay=1&amp;loop=1&amp;app_id=122963&amp;h=${tail}`}
+        frameBorder="0"
+        allow="autoplay; fullscreen; picture-in-picture"
+        allowFullScreen
+        style={IFRAME_STYLES}
+        title={title}
+        data-ready="true"
+      />
+    </div>
   );
 };
 
